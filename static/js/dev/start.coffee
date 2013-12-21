f28a59debb34007a5b43907b7d5943e3ddbca82f
Fernando Guirao <fernando@bueninvento.es>
2013-12-22 00:06:11 +0100

    skrollr añadido

--- static/js/dev/start.coffee
+++ static/js/dev/start.coffee
@@ -1,4 +1,15 @@
 $j = jQuery
 
 $j ->
-  # $j().ready ->
+$ ->
+  BV = new $.BigVideo(useFlashForFirefox: true)
+  BV.init()
+  if Modernizr.touch
+    BV.show "media/images/background-dock.jpg"
+  else
+    BV.show "media/videos/sexy.mp4",
+      ambient: true
+      altSource: "media/videos/sexy.ogv"
+
+
+  s = skrollr.init()
--- static/js/dev/start.js
+++ static/js/dev/start.js
@@ -5,4 +5,21 @@
 
   $j(function() {});
 
+  $(function() {
+    var BV, s;
+    BV = new $.BigVideo({
+      useFlashForFirefox: true
+    });
+    BV.init();
+    if (Modernizr.touch) {
+      BV.show("media/images/background-dock.jpg");
+    } else {
+      BV.show("media/videos/sexy.mp4", {
+        ambient: true,
+        altSource: "media/videos/sexy.ogv"
+      });
+    }
+    return s = skrollr.init();
+  });
+
 }).call(this);
